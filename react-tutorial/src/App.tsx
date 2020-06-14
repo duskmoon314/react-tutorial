becd7cf06b09faa18593f28f38553892b38a20f9
Campbell <hkp18@mails.tsinghua.edu.cn>
2020-06-14 21:00:08 +0800

    :sparkles: Add antd and build a frame

--- react-tutorial/src/App.tsx
+++ react-tutorial/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { InputNumber, message } from "antd";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [guess, setGuess] = useState<number>(0);
+
+  const onInputChange = (value: number | string | undefined) => {
+    message.info(`You guess ${value}`);
+    if (value) setGuess(parseInt(value.toString()));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Guessing Game</h1>
+      <p>Please Input Your Number:</p>
+      <InputNumber defaultValue={3} onChange={onInputChange} />
+      <p>You guess {guess}</p>
     </div>
   );
-}
+};
 
 export default App;
