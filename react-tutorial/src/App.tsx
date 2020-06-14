d172ec226553a966bb444df27afe952283e8dd86
Campbell <hkp18@mails.tsinghua.edu.cn>
2020-06-14 21:57:50 +0800

    :sparkles: use state to make it work

--- react-tutorial/src/App.tsx
+++ react-tutorial/src/App.tsx
@@ -1,21 +1,49 @@
 import React, { useState } from "react";
-import { InputNumber, message } from "antd";
+import { Button, InputNumber, message } from "antd";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [secret, setSecret] = useState(Math.floor(Math.random() * 100 + 1));
   const [guess, setGuess] = useState<number>(0);
+  const [randomAble, setRandomAble] = useState(false);
 
   const onInputChange = (value: number | string | undefined) => {
-    message.info(`You guess ${value}`);
     if (value) setGuess(parseInt(value.toString()));
+  };
+
+  const handleSubmit = () => {
+    if (guess < secret) message.info("Too small");
+    else if (guess > secret) message.info("Too big");
+    else {
+      message.success("You win");
+      setRandomAble(true);
+    }
+  };
+
+  const handleRandom = () => {
+    setSecret(Math.floor(Math.random() * 100 + 1));
+    setRandomAble(false);
   };
 
   return (
     <div>
       <h1>Guessing Game</h1>
       <p>Please Input Your Number:</p>
-      <InputNumber defaultValue={3} onChange={onInputChange} />
-      <p>You guess {guess}</p>
+      <InputNumber
+        defaultValue={3}
+        onChange={onInputChange}
+        onPressEnter={handleSubmit}
+      />
+      <Button onClick={handleSubmit} type="primary">
+        Submit
+      </Button>
+      <Button onClick={handleRandom} disabled={!randomAble}>
+        Another Guess
+      </Button>
+      <p>
+        You guess {guess}
+        {/*secret {secret}*/}
+      </p>
     </div>
   );
 };
